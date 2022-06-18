c9637cdfbda5d59f6e47a3670e30ab1b2cd522db
DenissLohins <d2602l7759>
2022-06-18 09:50:10 +0300

    Initial commit

---
+++ src/main/java/com/homework/CountryByPhoneNumber/CountryByPhoneNumberApplication.java
@@ -0,0 +1,13 @@
+package com.homework.CountryByPhoneNumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryByPhoneNumberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryByPhoneNumberApplication.class, args);
+	}
+
+}
